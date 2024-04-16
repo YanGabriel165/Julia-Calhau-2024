7d18392ada000f85b8e13c7078cf3b2f0585736d
YanGabriel165 <160656280+YanGabriel165@users.noreply.github.com>
2024-04-16 11:09:37 -0300

    Create Nascimento.C

---
+++ Nascimento.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main (){
+    
+    int ano_nascimento;
+    int situacao;
+    int ano = 2024;
+    
+    
+printf("Digite o nascimento da pessoa");
+scanf("%d", &ano_nascimento);
+
+situacao = ano - ano_nascimento;
+
+if(situacao >= 18){
+    printf("pode votar");
+    
+
+
+
+}else{
+   printf("Não pode votar");
+   
+}
+
+return 0;
+
+}
