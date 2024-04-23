aa11292a03f2833e892e75267dfd4793b42efa01
YanGabriel165 <160656280+YanGabriel165@users.noreply.github.com>
2024-04-23 10:01:00 -0300

    Create Crescente.C

---
+++ Crescente.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main () {
+  int a, b, c;
+  
+  printf("digite tres valores inteiros diferentes:\n");
+  scanf("%d %d %d", &a, &b, &c);
+  
+  if (a>b){
+      int temp = a;
+      a = b;
+      b = temp;
+      
+  }
+  if (b>c){
+      int temp = b;
+      b = c;
+      c = temp;
+      
+  }
+  if (a > b){
+      int temp = a;
+      a = b;
+      b = temp;
+      
+  }
+  
+  printf("Os valores em ordem crescente são: %d %d %d\n", a, b, c);
+}
